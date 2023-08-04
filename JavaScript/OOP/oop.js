afae7c79ccc008ab2e227f98aeafeeb7ec461fa0
Alie <abu.khater90@gmail.com>
2023-08-04 18:26:17 +0300

    OOP

---
+++ JavaScript/OOP/oop.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+class media {
+
+constructor ( title , duration ) {
+    this.title = title;
+    this.duration = duration ;
+    this.ratings = [];
+
+   addRating (rating ) 
+     { this.ratings.push(this.ratings) ; } ;
+
+     calculateAverageRating ()
+     {
+
+            if (this.ratings === 0 )
+            {
+                return " 'No ratings yet'"
+            } ; 
+
+
+     };
+
+
+}}
+
+
+
+
+
