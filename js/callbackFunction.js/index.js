23dbc40c6c6266b443877111aef1d68c76d4d848
amalshweiki <amalshweiki88@gmail.com>
2023-08-05 22:25:35 +0300

    Movie Rating SystemMovie Rating System

--- js/callbackFunction.js/index.js
+++ js/callbackFunction.js/index.js
@@ -8,8 +8,8 @@ function convertToUpperCase(movie, callback) {
     }
   }
   
-  let movie1 = { title: "Movie 1", genre: "action" }; 
-  convertToUpperCase(movie1, genre => genre.toUpperCase())
+  let myMovie = { title: "Movie 1", genre: "action" }; 
+  convertToUpperCase(myMovie, genre => genre.toUpperCase())
 
 
 
@@ -27,8 +27,8 @@ function convertToUpperCase(movie, callback) {
                                  }
     }
   }
-   movie1=  { title: "Movie 1", rating: 8 }; 
-   checkifRatingisNumber(movie1, isNumber)
+   myMovie=  { title: "Movie 1", rating: 8 }; 
+   checkifRatingisNumber(myMovie, isNumber)
 
 
    //Exercise 3: Add Exclamation to Movie Title
@@ -41,8 +41,8 @@ function convertToUpperCase(movie, callback) {
         console.log(movie)           
                                       }
 
-   movie1=  { title: "Movie 1", rating: 8 }; 
-   addExclamationtoMovieTitle(movie1, addExclamation)
+   myMovie=  { title: "Movie 1", rating: 8 }; 
+   addExclamationtoMovieTitle(myMovie, addExclamation)
 
    //Exercise 4: Reverse Words in Movie Description
 
@@ -54,5 +54,90 @@ function  reverseWordsInMovieDescription(movie,callback) {
     movie.description=callback(movie.description);
     console.log(movie)           
                                   }
-    movie1={ title: "Movie 1", description: "An exciting action thriller" };                            
-    reverseWordsInMovieDescription(movie1,reverseWords)
+    myMovie={ title: "Movie 1", description: "An exciting action thriller" };                            
+    reverseWordsInMovieDescription(myMovie,reverseWords)
+
+    
+const movie1 = {
+  id: 2,
+  title: "Movie title 1",
+  genres: ["Genre1", "Genre2"],
+  ratings: [8, 7, 10]
+};
+
+const movie2 = {
+  id: 3,
+  title: "Movie title 2",
+  genres: ["Genre1", "Genre2"],
+  ratings: [8, 7, 10]
+};
+
+const movie3 = {
+  id: 5,
+  title: "Movie title 3",
+  genres: ["Genre1", "Genre2"],
+  ratings: [8, 7, 10]
+};
+
+const movie4 = {
+  id: 5,
+  title: "Movie title 4",
+  genres: ["Genre1", "Genre2"],
+  ratings: [8, 7, 10]
+};
+
+const moviesArr = [movie1, movie2, movie3, movie4]
+
+function processMovies(movies, cb) {
+  if (!Array.isArray(movies)) {
+    throw new Error('Expected an array');
+  }
+
+  const newMovies = JSON.parse(JSON.stringify(movies));
+
+  for (let i = 0; i < newMovies.length; i++) {
+    if (newMovies[i].hasOwnProperty('title') && newMovies[i].hasOwnProperty('genres') && newMovies[i].hasOwnProperty('ratings')) {
+
+      newMovies[i] = cb(newMovies[i]);
+    } else {
+      throw new Error('Movie is not in the right structure');
+    }
+  }
+
+  return newMovies;
+}
+
+const newMovies = processMovies(moviesArr, (movie) => {
+  return {
+    ...movie,
+    title: movie.title.toUpperCase(),
+    reviews: []
+  }
+})
+console.log('---------------------------')
+console.log(moviesArr)
+console.log('---------------------------')
+console.log(newMovies)
+
+function processMovie(movie, cb) {
+  if (typeof movie !== "object") {
+    throw new Error('Expected a movie object');
+  }
+
+  if (movie.hasOwnProperty('title') && movie.hasOwnProperty('genres') && movie.hasOwnProperty('ratings')) {
+    const newMovie = JSON.parse(JSON.stringify(movie));
+
+    return cb(newMovie);
+  } else {
+    throw new Error('Movie is not in the right structure');
+  }
+}
+
+function addReviewsArr(movie) {
+  return {
+    ...movie,
+    reviews: []
+  };
+}
+
+const processedMovie = processMovie(movie1, addReviewsArr);
