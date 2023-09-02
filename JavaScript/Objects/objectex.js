963e5e25602c2e67cfa9f588a8b32df3e2425ee4
Ali Abu Khater <93696256+Abukhater90@users.noreply.github.com>
2023-09-03 02:15:25 +0300

    Update objectex.js

--- JavaScript/Objects/objectex.js
+++ JavaScript/Objects/objectex.js
@@ -26,10 +26,37 @@ const shoppingCart = {
      ],
      AddItems : function (){
 
+         const existingItem = this.items.find(item => item.name === name);
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      this.items.push({ name, price, quantity });
+    }
+  },
+
      }
         }
         
         
-     
-     
+     removeItem(name) {
+    // Find the index of the item with the given name
+    const index = this.items.findIndex(item => item.name === name);
 
+    if (index !== -1) {
+      // If the item exists, remove it from the cart
+      this.items.splice(index, 1);
+    }
+  },
+
+  calculateTotal() {
+    let total = 0;
+
+    // Loop through each item in the cart and calculate the subtotal
+    for (const item of this.items) {
+      total += item.price * item.quantity;
+    }
+
+    return total;
+  },
+};
