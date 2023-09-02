730f2f7531a1f86455b68c44a15d70b3b7b9bdac
Ali Abu Khater <93696256+Abukhater90@users.noreply.github.com>
2023-09-03 02:10:33 +0300

    Create objectex.js

---
+++ JavaScript/Objects/objectex.js
@@ -0,0 +1,35 @@
+
+
+
+const book = {
+title : "" ,
+author : "" ,
+isbn : "" ,
+isCheckOut : true,
+toggleCheckOutStatus: function (isCheckOut){
+
+} 
+
+}
+
+
+
+
+const shoppingCart = {
+    item : [
+        {
+            name : null,
+            price :null,
+            quantity : null ,
+            
+        } 
+     ],
+     AddItems : function (){
+
+     }
+        }
+        
+        
+     
+     
+
